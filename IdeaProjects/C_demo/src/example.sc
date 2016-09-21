83f0b74269d53c1516683a304305ac123519b8ed
PK <pk@koczyk.pl>
2016-09-21 14:53:51 +0200

    scala demo

--- IdeaProjects/C_demo/src/example.sc
+++ IdeaProjects/C_demo/src/example.sc
@@ -1,1 +1,1 @@
-"Hello, world"d
+"Hello, world"
---
+++ IdeaProjects/C_demo/src/main/scala/class_demo.scala
@@ -0,0 +1,11 @@
+/**
+  * Created by PKOC on 2016-09-21.
+  * class class_demo {
+
+}
+  */
+package example
+
+object class_demo extends App {
+  println("Hello World!")
+}
