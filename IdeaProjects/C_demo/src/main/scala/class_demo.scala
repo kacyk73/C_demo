f7cb8301b7ff12bd26c01afb48672ae28c7c4173
PK <pk@koczyk.pl>
2016-09-23 12:05:54 +0200

    scala demo

--- IdeaProjects/C_demo/src/main/scala/class_demo.scala
+++ IdeaProjects/C_demo/src/main/scala/class_demo.scala
@@ -4,8 +4,32 @@
 
 }
   */
-package example
 
-object class_demo extends App {
-  println("Hello W1ors4w4lde5g!")
+// In file ChecksumAccumulator.scala
+
+class ChecksumAccumulator {
+  private var sum = 0
+  def add(b: Byte): Unit = { sum += b }
+  def checksum(): Int = ~(sum & 0xFF) + 1
+}
+
+import scala.collection.mutable
+object ChecksumAccumulator {
+  private val cache = mutable.Map.empty[String, Int]
+  def calculate(s: String): Int =
+    if (cache.contains(s))
+      cache(s)
+    else {
+      val acc = new ChecksumAccumulator
+      for (c <- s)
+        acc.add(c.toByte)
+      val cs = acc.checksum()
+      cache += (s -> cs)
+      cs
+    }
+}
+//import ChecksumAccumulator.calculate
+object FallWinterSpringSummer extends App {
+  for (season <- List("fall", "winter", "spring"))
+    println(season + ": " + calculate(season))
 }
